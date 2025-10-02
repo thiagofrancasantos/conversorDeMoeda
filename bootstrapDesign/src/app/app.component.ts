2a8718566477950353bd4e00d9b37e345e0ab71c
Thiago Franca <francasantos.t@gmail.com>
2025-10-02 20:56:02 +0000

    "sidebar personalizado adicionado"

--- bootstrapDesign/src/app/app.component.ts
+++ bootstrapDesign/src/app/app.component.ts
@@ -5,8 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bootstrapDesign';
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
